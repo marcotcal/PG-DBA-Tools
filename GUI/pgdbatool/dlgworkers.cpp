1c86c14d834af0242895e35747bbe6377fa26988
Marco Túlio Castro <marco.castro@qvaliz.pt>
2019-11-09 16:27:09 +0100

    Workers Dialog

---
+++ GUI/pgdbatool/dlgworkers.cpp
@@ -0,0 +1,14 @@
+#include "dlgworkers.h"
+#include "ui_dlgworkers.h"
+
+DlgWorkers::DlgWorkers(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::DlgWorkers)
+{
+    ui->setupUi(this);
+}
+
+DlgWorkers::~DlgWorkers()
+{
+    delete ui;
+}
