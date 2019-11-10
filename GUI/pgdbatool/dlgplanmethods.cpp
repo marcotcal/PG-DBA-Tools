c9e3d1839328c29666c9cbf0329ca6ae6ce70fc8
Marco Túlio Castro <marco.castro@qvaliz.pt>
2019-11-10 10:05:06 +0100

    Planner dialog implemented

--- GUI/pgdbatool/dlgplanmethods.cpp
+++ GUI/pgdbatool/dlgplanmethods.cpp
@@ -12,3 +12,85 @@ DlgPlanMethods::~DlgPlanMethods()
 {
     delete ui;
 }
+
+void DlgPlanMethods::setPlanFlags(int flags)
+{
+    ui->ck_bitmap_scan->setChecked((flags & PL_BITMAP_SCAN) == PL_BITMAP_SCAN);
+    ui->ck_hash_aggregations->setChecked((flags & PL_HASH_AGGREGATIONS) == PL_HASH_AGGREGATIONS);
+    ui->ck_hash_joins->setChecked((flags & PL_HASH_JOINS) == PL_HASH_JOINS);
+    ui->ck_index_scan->setChecked((flags & PL_INDEX_SCAN) == PL_INDEX_SCAN);
+    ui->ck_index_only_scan->setChecked((flags & PL_INDEX_ONLY_SCAN) == PL_INDEX_ONLY_SCAN);
+    ui->ck_materialization->setChecked((flags & PL_MATERIALIZATION) == PL_MATERIALIZATION);
+    ui->ck_nested_loops->setChecked((flags & PL_NESTED_LOOPS) == PL_NESTED_LOOPS);
+    ui->ck_sequential_scan->setChecked((flags & PL_SEQUENTIAL_SCAN) == PL_SEQUENTIAL_SCAN);
+    ui->ck_sort_steps->setChecked((flags & PL_SORT_STEPS) == PL_SORT_STEPS);
+    ui->ck_tdi_scan->setChecked((flags & PL_TDI_SCAN) == PL_TDI_SCAN);
+}
+
+void DlgPlanMethods::setSequentialPageCost(double cost)
+{
+    ui->sp_seq_page_cost->setValue(cost);
+}
+
+void DlgPlanMethods::setRandomPageCost(double cost)
+{
+    ui->sp_random_page_cost->setValue(cost);
+}
+
+void DlgPlanMethods::setTupleCost(double cost)
+{
+    ui->sp_cpu_tuple_cost->setValue(cost);
+}
+
+void DlgPlanMethods::setOperatorCost(double cost)
+{
+    ui->sp_operator_cost->setValue(cost);
+}
+
+void DlgPlanMethods::setEfectiveCashSize(int size)
+{
+    ui->sp_efective_cache_size->setValue(size);
+}
+
+int DlgPlanMethods::getPlanFlags()
+{
+    int flags = 0;
+
+    flags += ui->ck_bitmap_scan->isChecked() ? PL_BITMAP_SCAN : 0;
+    flags += ui->ck_hash_aggregations->isChecked() ? PL_HASH_AGGREGATIONS : 0;
+    flags += ui->ck_hash_joins->isChecked() ? PL_HASH_JOINS : 0;
+    flags += ui->ck_index_scan->isChecked() ? PL_INDEX_SCAN : 0;
+    flags += ui->ck_index_only_scan->isChecked() ? PL_INDEX_ONLY_SCAN : 0;
+    flags += ui->ck_materialization->isChecked() ? PL_MATERIALIZATION : 0;
+    flags += ui->ck_nested_loops->isChecked() ? PL_NESTED_LOOPS : 0;
+    flags += ui->ck_sequential_scan->isChecked() ? PL_SEQUENTIAL_SCAN : 0;
+    flags += ui->ck_sort_steps->isChecked() ? PL_SORT_STEPS : 0;
+    flags += ui->ck_tdi_scan->isChecked() ? PL_TDI_SCAN : 0;
+
+    return flags;
+}
+
+double DlgPlanMethods::getSequetialPageCost()
+{
+    return ui->sp_seq_page_cost->value();
+}
+
+double DlgPlanMethods::getRandomPageCost()
+{
+    return ui->sp_random_page_cost->value();
+}
+
+double DlgPlanMethods::getTupleCost()
+{
+    return ui->sp_cpu_tuple_cost->value();
+}
+
+double DlgPlanMethods::getOperatorCost()
+{
+    return ui->sp_operator_cost->value();
+}
+
+int DlgPlanMethods::getEfectivenessCashSize()
+{
+    return ui->sp_efective_cache_size->value();
+}
--- GUI/pgdbatool/dlgplanmethods.h
+++ GUI/pgdbatool/dlgplanmethods.h
@@ -12,9 +12,32 @@ class DlgPlanMethods : public QDialog
     Q_OBJECT
 
 public:
+    enum {
+        PL_BITMAP_SCAN = 1,
+        PL_HASH_AGGREGATIONS = 3,
+        PL_HASH_JOINS = 7,
+        PL_INDEX_SCAN = 15,
+        PL_INDEX_ONLY_SCAN = 31,
+        PL_MATERIALIZATION = 63,
+        PL_NESTED_LOOPS = 127,
+        PL_SEQUENTIAL_SCAN = 255,
+        PL_SORT_STEPS = 511,
+        PL_TDI_SCAN = 1023
+    };
     explicit DlgPlanMethods(QWidget *parent = nullptr);
     ~DlgPlanMethods();
-
+    void setPlanFlags(int flags);
+    void setSequentialPageCost(double cost);
+    void setRandomPageCost(double cost);
+    void setTupleCost(double cost);
+    void setOperatorCost(double cost);
+    void setEfectiveCashSize(int size);
+    int getPlanFlags();
+    double getSequetialPageCost();
+    double getRandomPageCost();
+    double getTupleCost();
+    double getOperatorCost();
+    int getEfectivenessCashSize();
 private:
     Ui::DlgPlanMethods *ui;
 };
