cdcac088ce30108ac61e2957b282e3eb9a055dda
marcotc_al <marcotc.al@gmail.com>
2020-02-19 22:54:14 +0100

    Config parameters

--- GUI/pgdbatool/dlgconfiguration.cpp
+++ GUI/pgdbatool/dlgconfiguration.cpp
@@ -14,6 +14,7 @@ DlgConfiguration::DlgConfiguration(bool ready_only, QWidget *parent) :
         ui->bt_open_path_sql->setEnabled(false);
         ui->bt_open_path_models->setEnabled(false);
     }
+    ui->tabs->setCurrentIndex(0);
 }
 
 DlgConfiguration::~DlgConfiguration()
@@ -31,6 +32,33 @@ QString DlgConfiguration::getPathToSql()
     return ui->path_to_sql->text();
 }
 
+int DlgConfiguration::getOptions()
+{
+    int value = 0;
+
+    if (ui->include_task_code->isChecked())
+        value += SCG_INCLUDE_TASK_CODE;
+
+    if (ui->include_sprint_code->isChecked())
+        value += SCG_INCLUDE_SPRINT_CODE;
+
+    if (ui->serialize_by_date->isChecked())
+        value += SCG_SERIALIZE_BY_DATE;
+
+    if (ui->serialize_by_integer->isChecked())
+        value += SCG_SERIALIZE_BY_INTEGER;
+
+    if (ui->keep_script_on_review->isChecked())
+        value += SCG_KEEP_ON_REVIEW;
+
+    return value;
+}
+
+QString DlgConfiguration::getTaskPrefix()
+{
+    return ui->task_code_prefix->text();
+}
+
 void DlgConfiguration::setPathToModels(QString value)
 {
     ui->path_to_query_models->setText(value);
@@ -39,6 +67,20 @@ void DlgConfiguration::setPathToModels(QString value)
 void DlgConfiguration::setPathToSql(QString value)
 {
     ui->path_to_sql->setText(value);
+}
+
+void DlgConfiguration::setOptions(int value)
+{
+    ui->include_task_code->setChecked((value & SCG_INCLUDE_TASK_CODE));
+    ui->include_sprint_code->setChecked((value & SCG_INCLUDE_SPRINT_CODE));
+    ui->keep_script_on_review->setChecked((value & SCG_KEEP_ON_REVIEW));
+    ui->serialize_by_date->setChecked((value & SCG_SERIALIZE_BY_DATE));
+    ui->serialize_by_integer->setChecked((value & SCG_SERIALIZE_BY_INTEGER));
+}
+
+void DlgConfiguration::setTaskPrefix(QString value)
+{
+    ui->task_code_prefix->setText(value);
 }
 
 void DlgConfiguration::on_bt_open_path_models_clicked()
--- GUI/pgdbatool/dlgconfiguration.h
+++ GUI/pgdbatool/dlgconfiguration.h
@@ -12,12 +12,28 @@ class DlgConfiguration : public QDialog
     Q_OBJECT
 
 public:
+
+    enum {
+        SCG_INCLUDE_TASK_CODE = 1,
+        SCG_INCLUDE_SPRINT_CODE = 2,
+        SCG_KEEP_ON_REVIEW = 4,
+        SCG_SERIALIZE_BY_DATE = 8,
+        SCG_SERIALIZE_BY_INTEGER = 16
+    };
+
     explicit DlgConfiguration(bool ready_only = false, QWidget *parent = nullptr);
     ~DlgConfiguration();
+
     QString getPathToModels();
     QString getPathToSql();
+    int getOptions();
+    QString getTaskPrefix();
+
     void setPathToModels(QString value);
     void setPathToSql(QString value);
+    void setOptions(int value);
+    void setTaskPrefix(QString value);
+
 private slots:
     void on_bt_open_path_models_clicked();
     void on_bt_open_path_sql_clicked();
