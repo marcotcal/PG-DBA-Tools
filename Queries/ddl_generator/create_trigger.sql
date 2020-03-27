76de506cde8b6b4e3d51c4446de938812229aac7
marcotcal <marcotc.al@gmail.com>
2020-03-27 20:49:22 +0100

    DDL Scripts

--- Queries/ddl_generator/create_trigger.sql
+++ Queries/ddl_generator/create_trigger.sql
@@ -1,36 +1,34 @@
 WITH
-triggers as (
-    SELECT trg.tgname AS trigger_name,
-           tbl.relname AS table_name,
-           p.proname AS function_name,
-           CASE trg.tgtype & cast(2 as int2)
-             WHEN 0 THEN 'AFTER'
-             ELSE 'BEFORE'
-           END AS trigger_type,
-           CASE trg.tgtype & cast(28 as int2)
-             WHEN 16 THEN 'UPDATE'
-             WHEN  8 THEN 'DELETE'
-             WHEN  4 THEN 'INSERT'
-             WHEN 20 THEN 'INSERT OR UPDATE'
-             WHEN 28 THEN 'INSERT OR UPDATE OR DELETE'
-             WHEN 24 THEN 'UPDATE OR DELETE'
-             WHEN 12 THEN 'INSERT OR DELETE'
-           END AS trigger_event,
-           CASE trg.tgtype & cast(1 as int2)
-             WHEN 0 THEN 'FOR EACH STATEMENT'
-             ELSE 'FOR EACH ROW'
-           END AS action_orientation
-      FROM pg_trigger trg,
-           pg_class tbl,
-           pg_proc p
-     WHERE trg.tgrelid = tbl.oid
-       AND trg.tgfoid = p.oid
-       AND tbl.relname !~ '^pg_'
+    triggers as (
+        SELECT 
+            pg_get_triggerdef(trg.oid) AS create_trigger
+        FROM 
+            pg_trigger trg
+            INNER JOIN pg_class tbl ON trg.tgrelid = tbl.oid
+            INNER JOIN pg_namespace n ON tbl.relnamespace = n.oid
+        WHERE        
+            tbl.relname !~ '^pg_'
+            AND tbl.relname  =  'clinic' 
+            AND n.nspname = 'clinics'
+            --AND tgisinternal = FALSE
     )   
-    SELECT 
-        'CREATE TRIGGER ' || t.trigger_name || E'\n    ' || 
-        t.trigger_type || ' ' || t.trigger_event || E'\n' ||
-        '    ON ' || t.table_name || E'\n' || 
-        '    ' || t.action_orientation || E'\n' || 
-        '    EXECUTE PROCEDURE ' || t.function_name         
+    SELECT  
+        replace(
+            replace(
+                replace(
+                    replace(
+                        replace(
+                            replace(
+                                replace(
+                                    replace(
+                                        replace(create_trigger, 'BEFORE ', E'\n    BEFORE '
+                                        ), 'AFTER', E'\n    AFTER'
+                                    ), 'ON ', E'\n    ON '       
+                                ), 'FROM', E'\n    FROM'
+                            ), 'NOT DEFERRABLE', E'\n    NOT DEFERRABLE'
+                        ), 'INITIALLY', E'\n    INITIALLY'  
+                    ), 'FOR ', E'\n    FOR '
+                ), 'EXECUTE', E'\n    EXECUTE'
+            ), 'WHEN ', E'\n    WHEN '  
+        )    
     FROM triggers t
