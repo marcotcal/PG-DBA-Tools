826b4169390ab9f5c8fa9ce61e8426206ba6fcbf
marcotcal <marcotc.al@gmail.com>
2026-08-06 14:03:19 +0100

    Test main

--- agent/cmd/pgdbagent/main.go
+++ agent/cmd/pgdbagent/main.go
@@ -1,7 +1,28 @@
-/*******************************************************************************
-   PGDBATOOLS AGENT
+/***************************************************************************
+ *  PGDBATOOLS AGENT                                                        
+ *
+ *  START  : 2026-08-06
+ *  AUTHOR : Marco Túlio Castro
+ ***************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
-   START  : 2026-08-06
-   AUTHOR : Marco Túlio Castro
-********************************************************************************/
+package main
 
+import (
+	//"context"
+	"fmt"
+	//"log"
+	//"github.com/jackc/pgx/v5"
+)
+
+func main() {
+
+	fmt.Println("PG DBA Tools Agent")
+}
